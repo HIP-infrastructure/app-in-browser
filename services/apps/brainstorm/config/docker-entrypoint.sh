00e26fc30e2ae40323ba27a23337507554274d4b
Nathalie Casati <nathalie.casati@chuv.ch>
2021-03-16 22:05:06 +0100

    fix opengl error in brainstorm container, refactored to allow common scripts

--- services/apps/brainstorm/config/docker-entrypoint.sh
+++ services/apps/brainstorm/config/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # based on https://github.com/ffeldhaus/docker-xpra-html5-gpu-minimal/blob/master/docker-entrypoint.sh
-if [ ! -d "/dev/dri" ];then
-  echo "ERROR: Directory /dev/dri does not exists, likely the GPU is not available to container."
-  exit 1
+/home/hip-user/scripts/check-dri.sh
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
 fi
 
 # ensure that xpra user is part of vglusers group which must have been set for /dev/dri/card0
@@ -12,11 +13,10 @@ groupadd -f -g $DRI_CARD_GID $DRI_CARD_GROUP_NAME
 DRI_RENDER_GROUP_NAME=$(ls -l /dev/dri/render* | head -1 | awk '{print $4}')
 DRI_RENDER_GID=$(ls -ln /dev/dri/render* | head -1 | awk '{print $4}')
 groupadd -f -g $DRI_RENDER_GID $DRI_RENDER_GROUP_NAME
-usermod --groups $DRI_CARD_GID,$DRI_RENDER_GID --append xpra
-chmod -R 1777 /tmp/.X11-unix/
-rm -rf /tmp/.X80-lock
+usermod --groups $DRI_CARD_GID,$DRI_RENDER_GID --append hip-user
 
-# start xpra as xpra user with command specified in dockerfile as CMD or passed as parameter to docker run
-#CMD="XPRA_PASSWORD=$XPRA_PASSWORD /usr/bin/xpra start --daemon=no --start-child='$@'"
-runuser -l xpra -c 'xpra start :80 --bind-tcp=0.0.0.0:8080 --html=on --no-daemon --start="xhost +"'
-#runuser -l xpra -c 'sleep 1000000000000'
+#run brainstorm as hip-user
+#CMD="DISPLAY=:80 vglrun -d /dev/dri/card1 /opt/VirtualGL/bin/glxspheres64"
+CMD="DISPLAY=:80 vglrun -d /dev/dri/card1 /home/hip-user/apps/brainstorm/run/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98"
+runuser -l hip-user -c "$CMD"
+#runuser -l hip-user -c 'sleep 1000000000000'
---
+++ services/scripts/check-dri.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ ! -d "/dev/dri" ];then
+  echo "ERROR: Directory /dev/dri does not exists, likely the GPU is not available to container."
+  exit 1
+fi
+  echo "SUCCESS: Directory /dev/dri exists, likely the GPU is available to container."
