92b4d9c7980acc9d36dc7b9c3bb7ef6c84e80eac
Nathalie Casati <nathalie.casati@chuv.ch>
2021-04-12 18:36:52 +0000

    added pip3 install to script

--- scripts/installbackend.sh
+++ scripts/installbackend.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+if ! command -v pip3 &> /dev/null
+then
+    echo "pip3 could not be found, installing..."
+    sudo apt-get update && sudo apt-get install -y python3-pip
+    echo "pip3 installed."
+fi
 sudo pip3 install -r backend/requirements.txt
