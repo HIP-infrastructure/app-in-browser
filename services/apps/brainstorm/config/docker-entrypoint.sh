44b2e21948220f08f7262d9ef27626ae9c566006
Nathalie Casati <nathalie.casati@chuv.ch>
2021-04-13 20:47:48 +0200

    dl brainstorm in image and refactoring

--- services/apps/brainstorm/config/docker-entrypoint.sh
+++ services/apps/brainstorm/config/docker-entrypoint.sh
@@ -29,6 +29,6 @@ chown -R $HIP_USER:1000 /home/$HIP_USER/brainstorm_db
 
 #run brainstorm as $HIP_USER
 #CMD="DISPLAY=:80 vglrun -d /dev/dri/card1 /opt/VirtualGL/bin/glxspheres64"
-CMD="DISPLAY=:80 vglrun -d /dev/dri/card1 /apps/brainstorm/run/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98"
+CMD="DISPLAY=:80 vglrun -d /dev/dri/card1 /apps/brainstorm/install/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98"
 runuser -l $HIP_USER -c "$CMD"
 #runuser -l $HIP_USER -c 'sleep 1000000000000'
