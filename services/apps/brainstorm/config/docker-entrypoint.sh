ad134813b5eaf51868aedc0b34a7806cc0e0fe29
Nathalie Casati <nathalie.casati@chuv.ch>
2021-05-05 22:49:04 +0200

    tried lots to mount .brainstorm and mcrcache from nc but failed

--- services/apps/brainstorm/config/docker-entrypoint.sh
+++ services/apps/brainstorm/config/docker-entrypoint.sh
@@ -29,10 +29,42 @@ if [ $retVal -ne 0 ]; then
 fi
 
 #symlink brainstorm_db in $HIP_USER homedir
-ln -sf /apps/brainstorm/run/brainstorm_db /home/$HIP_USER
-chown -R $HIP_USER:1000 /home/$HIP_USER/brainstorm_db
+#ln -sf /apps/brainstorm/run/brainstorm_db /home/$HIP_USER
+#chown -R $HIP_USER:1000 /home/$HIP_USER/brainstorm_db
+echo -n "Symlinking brainstorm_db from davfs2... "
+mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db
+chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db
+ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db /home/$HIP_USER
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
+chown -R $HIP_USER:davfs2 /home/$HIP_USER/brainstorm_db
+echo "done."
+
+#symlink .brainstorm in $HIP_USER homedir
+#echo -n "Symlinking .brainstorm from davfs2... "
+#mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm
+#chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm
+#ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm /home/$HIP_USER
+#if [ $retVal -ne 0 ]; then
+#  exit $retVal
+#fi
+#chown -R $HIP_USER:davfs2 /home/$HIP_USER/.brainstorm
+#echo "done."
+
+#symlink .mcrCache9.8 in $HIP_USER homedir
+#echo -n "Symlinking .mcrCache9.8 from davfs2... "
+#mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8
+#chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8
+#ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8 /home/$HIP_USER
+#if [ $retVal -ne 0 ]; then
+#  exit $retVal
+#fi
+#chown -R $HIP_USER:davfs2 /home/$HIP_USER/.mcrCache9.8
+#echo "done."
 
 #run brainstorm as $HIP_USER
+echo "Running brainstorm as $HIP_USER... "
 #CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD /opt/VirtualGL/bin/glxspheres64"
 CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD /apps/brainstorm/install/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98"
 runuser -l $HIP_USER -c "$CMD"
