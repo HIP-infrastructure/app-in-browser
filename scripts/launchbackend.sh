a7db10f22c0c8092434aafcb711724983f4c703b
Nathalie Casati <nathalie.casati@chuv.ch>
2021-06-02 18:02:09 +0000

    added reverse proxy and SSL support for thehip.app

--- scripts/launchbackend.sh
+++ scripts/launchbackend.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo gunicorn --workers 2 --timeout 120 --bind 0.0.0.0:8060 --pythonpath backend backend:app
+sudo caddy run -config caddy/Caddyfile &
+caddy=$!
+sudo gunicorn --workers 2 --timeout 120 --bind 0.0.0.0:8060 --pythonpath backend backend:app &
+gunicorn=$!
+wait $caddy $gunicorn
