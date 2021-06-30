3a6283ba5bb551f35c4afafe5a70cbbd40a3bf24
Nathalie Casati <nathalie.casati@chuv.ch>
2021-06-30 12:27:17 +0000

    added scripts for running apps

--- services/apps/brainstorm/config/docker-entrypoint.sh
+++ services/apps/brainstorm/config/docker-entrypoint.sh
@@ -27,50 +27,23 @@ if [ $retVal -ne 0 ]; then
   exit $retVal
 fi
 
-#symlink brainstorm_db in $HIP_USER homedir
+#old way without nextcloud
 #ln -sf /apps/brainstorm/run/brainstorm_db /home/$HIP_USER
 #chown -R $HIP_USER:1000 /home/$HIP_USER/brainstorm_db
-echo -n "Symlinking brainstorm_db from davfs2... "
-mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db
-chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db
-ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/brainstorm_db /home/$HIP_USER
+
+#symlink app_data directories in $HIP_USER homedir
+DIR_ARRAY=( "brainstorm/brainstorm_db" "brainstorm/.brainstorm" )
+#DIR_ARRAY=( "brainstorm/brainstorm_db" "brainstorm/.brainstorm" "brainstorm/.mcrCache9.8" )
+$SCRIPT_PATH/homedir-symlink.sh $HIP_USER ${DIR_ARRAY[@]}
+retVal=$?
 if [ $retVal -ne 0 ]; then
   exit $retVal
 fi
-chown -R $HIP_USER:davfs2 /home/$HIP_USER/brainstorm_db
-echo "done."
-
-#symlink .brainstorm in $HIP_USER homedir
-echo -n "Symlinking .brainstorm from davfs2... "
-mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm
-chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm
-ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/.brainstorm /home/$HIP_USER
-if [ $retVal -ne 0 ]; then
-  exit $retVal
-fi
-chown -R $HIP_USER:davfs2 /home/$HIP_USER/.brainstorm
-echo "done."
-
-#symlink .mcrCache9.8 in $HIP_USER homedir
-#echo -n "Symlinking .mcrCache9.8 from davfs2... "
-#mkdir -p /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8
-#chown -R $HIP_USER:davfs2 /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8
-#ln -sf /home/$HIP_USER/nextcloud/app_data/brainstorm/.mcrCache9.8 /home/$HIP_USER
-#if [ $retVal -ne 0 ]; then
-#  exit $retVal
-#fi
-#chown -R $HIP_USER:davfs2 /home/$HIP_USER/.mcrCache9.8
-#echo "done."
 
 #run brainstorm as $HIP_USER
-echo -n "Running brainstorm as $HIP_USER "
-if [ $CARD == "none" ]; then
-  echo "on CPU... "
-  CMD="DISPLAY=:80 /apps/brainstorm/install/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98 && /usr/sbin/umount.davfs /home/$HIP_USER/nextcloud"
-else
-  echo "on GPU... "
-  #CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD /opt/VirtualGL/bin/glxspheres64"
-  CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD /apps/brainstorm/install/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98 && /usr/sbin/umount.davfs /home/$HIP_USER/nextcloud"
+APP="/apps/brainstorm/install/brainstorm3/bin/R2020a/brainstorm3.command /usr/local/MATLAB/MATLAB_Runtime/v98"
+$SCRIPT_PATH/run-app.sh $CARD $HIP_USER $APP
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
 fi
-runuser -l $HIP_USER -c "$CMD"
-#runuser -l $HIP_USER -c 'sleep 1000000000000'
---
+++ services/scripts/run-app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CARD=$1
+HIP_USER=$2
+APP=$3
+
+#run $APP as $HIP_USER
+echo -n "Running brainstorm as $HIP_USER "
+if [ $CARD == "none" ]; then
+  echo "on CPU... "
+  CMD="DISPLAY=:80 $APP && /usr/sbin/umount.davfs /home/$HIP_USER/nextcloud"
+else
+  echo "on GPU... "
+  #CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD /opt/VirtualGL/bin/glxspheres64"
+  CMD="DISPLAY=:80 vglrun -d /dev/dri/$CARD $APP && /usr/sbin/umount.davfs /home/$HIP_USER/nextcloud"
+fi
+runuser -l $HIP_USER -c "$CMD"
+#runuser -l $HIP_USER -c 'sleep 1000000000000'
