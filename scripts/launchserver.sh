82a2977ba01c78796e35625f423687b0e36ce821
Nathalie Casati <nathalie.casati@chuv.ch>
2021-07-27 13:17:01 +0000

    removed docker-compose from server

--- scripts/launchserver.sh
+++ scripts/launchserver.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
+#source .env
+set -o allexport; source .env; set +o allexport
+
 SERVER_ID=$1
 HIP_USER=$2
 NAME=${SERVER_ID}-${HIP_USER}
-
-#source .env
-set -o allexport; source .env; set +o allexport
 
 #create volume
 docker volume create ${NAME}_x11-unix
 
 #create network
 docker network create -d bridge ${NAME}_server
+docker network create --internal --opt com.docker.network.driver.mtu=${MTU} ${NAME}_apps
 
 #run container
 docker run \
   -p "127.0.0.1::8080" \
   -v ${NAME}_x11-unix:/tmp/.X11-unix \
+  --network=${NAME}_server \
   --device=/dev/dri:/dev/dri \
   --runtime	${RUNTIME} \
   --name xpra-server-${NAME} \
   --hostname xpra-server-${NAME} \
-  --restart unless-stopped \
+  --restart on-failure \
   --env-file .env \
   --env NVIDIA_VISIBLE_DEVICES=all \
   --env NVIDIA_DRIVER_CAPABILITIES=all \
