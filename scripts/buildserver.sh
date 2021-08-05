2c49897f0e7c8e7333051874414cbbaaa68d8b23
Nathalie Casati <nathalie.casati@chuv.ch>
2021-08-05 13:42:39 +0000

    docker registry tag and push

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -10,3 +10,9 @@ docker build \
   --build-arg VIRTUALGL_VERSION=${VIRTUALGL_VERSION} \
   -t xpra-server:latest \
   -f ${CONTEXT}/server/Dockerfile ${CONTEXT}
+
+#push to registry during CI only
+if [ ! -z $CI_REGISTRY ]; then
+  REGISTRY_IMAGE=$CI_REGISTRY/$CI_REGISTRY_IMAGE:latest
+  docker tag xpra-server:latest $REGISTRY_IMAGE
+  docker push $REGISTRY_IMAGE
