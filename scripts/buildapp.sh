0904c9df2c5c34fe2f53875172d1ae7577bed77d
Nathalie Casati <nathalie.casati@chuv.ch>
2021-08-05 19:55:36 +0000

    docker registry tag and push

--- scripts/buildapp.sh
+++ scripts/buildapp.sh
@@ -3,19 +3,38 @@
 #source .env
 set -o allexport; source .env; set +o allexport
 
-CONTEXT=./services
+CONTEXT="./services"
 APP_NAME=$1
 APP_VERSION=${APP_NAME^^}_VERSION
 
 #build ${APP_NAME}
 if [ -z ${!APP_VERSION} ]; then
+  IMAGE=${APP_NAME}:latest
   docker build \
-  -t ${APP_NAME}:latest \
+  -t ${IMAGE} \
   -f ${CONTEXT}/apps/${APP_NAME}/Dockerfile ${CONTEXT}
+
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    exit $retVal
+  fi
 else
+  IMAGE=${APP_NAME}:${!APP_VERSION}
   docker build \
-  -t ${APP_NAME}:${!APP_VERSION} \
+  -t ${IMAGE} \
   --build-arg ${APP_VERSION}=${!APP_VERSION} \
   --build-arg DAVFS2_VERSION=${DAVFS2_VERSION} \
   -f ${CONTEXT}/apps/${APP_NAME}/Dockerfile ${CONTEXT}
+
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    exit $retVal
+  fi
+fi
+
+#push ${APP_NAME} to registry during CI only
+if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/${IMAGE}
+  docker tag ${IMAGE} ${REGISTRY_IMAGE}
+  docker push ${REGISTRY_IMAGE}
 fi
