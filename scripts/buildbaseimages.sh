49a2e866a109a6d16f751f8eab3cb04ad6be141d
Nathalie Casati <nathalie.casati@chuv.ch>
2021-08-05 14:19:31 +0000

    docker registry tag and push

--- scripts/buildbaseimages.sh
+++ scripts/buildbaseimages.sh
@@ -3,13 +3,25 @@
 #source .env
 set -o allexport; source .env; set +o allexport
 
-CONTEXT=./services
+CONTEXT="./services"
 
 #build vgl-base
 docker build \
   --build-arg VIRTUALGL_VERSION=${VIRTUALGL_VERSION} \
   -t vgl-base:${VIRTUALGL_VERSION} \
-  -f ${CONTEXT}/base-images/vgl-base/Dockerfile ${CONTEXT} &&
+  -f ${CONTEXT}/base-images/vgl-base/Dockerfile ${CONTEXT}
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
+
+#push vgl-base to registry during CI only
+if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/vgl-base:${VIRTUALGL_VERSION}
+  docker tag vgl-base:${VIRTUALGL_VERSION} ${REGISTRY_IMAGE}
+  docker push ${REGISTRY_IMAGE}
+fi
 
 #build nc-webdav
 docker build \
@@ -17,6 +29,18 @@ docker build \
   --build-arg DAVFS2_VERSION=${DAVFS2_VERSION} \
   -t nc-webdav:${DAVFS2_VERSION} \
   -f ${CONTEXT}/base-images/nc-webdav/Dockerfile ${CONTEXT} &&
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
+
+#push nc-webdav to registry during CI only
+if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/nc-webdav:${DAVFS2_VERSION}
+  docker tag nc-webdav:${DAVFS2_VERSION} ${REGISTRY_IMAGE}
+  docker push ${REGISTRY_IMAGE}
+fi
 
 #build each matlab-runtime
 for i in "${!MATLAB_RUNTIME_VERSIONS[@]}"; do
@@ -26,4 +50,16 @@ for i in "${!MATLAB_RUNTIME_VERSIONS[@]}"; do
     --build-arg MATLAB_RUNTIME_UPDATE=${MATLAB_RUNTIME_UPDATES[i]} \
     -t matlab-runtime:${MATLAB_RUNTIME_VERSIONS[i]}_u${MATLAB_RUNTIME_UPDATES[i]} \
     -f ${CONTEXT}/base-images/matlab-runtime/Dockerfile ${CONTEXT}
+
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    exit $retVal
+  fi
+
+  #push matlab-runtime to registry during CI only
+  if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+    REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/matlab-runtime:${MATLAB_RUNTIME_VERSIONS[i]}_u${MATLAB_RUNTIME_UPDATES[i]}
+    docker tag matlab-runtime:${MATLAB_RUNTIME_VERSIONS[i]}_u${MATLAB_RUNTIME_UPDATES[i]} ${REGISTRY_IMAGE}
+    docker push ${REGISTRY_IMAGE}
+  fi
 done
--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -3,17 +3,23 @@
 #source .env
 set -o allexport; source .env; set +o allexport
 
-CONTEXT=./services
+CONTEXT="./services"
+IMAGE="xpra-server:latest"
 
 #build xpra-server
 docker build \
   --build-arg VIRTUALGL_VERSION=${VIRTUALGL_VERSION} \
-  -t xpra-server:latest \
+  -t ${IMAGE} \
   -f ${CONTEXT}/server/Dockerfile ${CONTEXT}
 
-#push to registry during CI only
-if [ ! -z ${CI_REGISTRY} ]; then
-  REGISTRY_IMAGE=${CI_REGISTRY}/${CI_REGISTRY_IMAGE}:latest
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
+
+#push xpra-server to registry during CI only
+if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/${IMAGE}
   docker tag xpra-server:latest ${REGISTRY_IMAGE}
   docker push ${REGISTRY_IMAGE}
 fi
