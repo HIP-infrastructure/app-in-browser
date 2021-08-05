22cff9e1b13d0fbc897b4e679225ff6d94426f28
Nathalie Casati <nathalie.casati@chuv.ch>
2021-08-05 13:51:59 +0000

    docker registry tag and push

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -12,8 +12,8 @@ docker build \
   -f ${CONTEXT}/server/Dockerfile ${CONTEXT}
 
 #push to registry during CI only
-if [ ! -z $CI_REGISTRY ]; then
-  REGISTRY_IMAGE=$CI_REGISTRY/$CI_REGISTRY_IMAGE:latest
-  docker tag xpra-server:latest $REGISTRY_IMAGE
-  docker push $REGISTRY_IMAGE
+if [ ! -z ${CI_REGISTRY} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY}/${CI_REGISTRY_IMAGE}:latest
+  docker tag xpra-server:latest ${REGISTRY_IMAGE}
+  docker push ${REGISTRY_IMAGE}
 fi
