f60a436a03d9d271dfb65bca6cde6de59a24680a
Nathalie Casati <nathalie.casati@chuv.ch>
2021-08-06 08:28:15 +0000

    using cache

--- scripts/buildserver.sh
+++ scripts/buildserver.sh
@@ -6,9 +6,17 @@ set -o allexport; source .env; set +o allexport
 CONTEXT="./services"
 IMAGE="xpra-server:latest"
 
+#pull image and cache from registry during CI only
+if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
+  REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/${IMAGE}
+  docker pull ${REGISTRY_IMAGE} || true
+  CACHE_OPTS="--cache-from ${REGISTRY_IMAGE}"
+fi
+
 #build xpra-server
 docker build \
   --build-arg VIRTUALGL_VERSION=${VIRTUALGL_VERSION} \
+  ${CACHE_OPTS} \
   -t ${IMAGE} \
   -f ${CONTEXT}/server/Dockerfile ${CONTEXT}
 
@@ -20,6 +28,6 @@ fi
 #push xpra-server to registry during CI only
 if [ ! -z ${CI_REGISTRY_IMAGE} ]; then
   REGISTRY_IMAGE=${CI_REGISTRY_IMAGE}/${IMAGE}
-  docker tag xpra-server:latest ${REGISTRY_IMAGE}
+  docker tag ${IMAGE} ${REGISTRY_IMAGE}
   docker push ${REGISTRY_IMAGE}
 fi
