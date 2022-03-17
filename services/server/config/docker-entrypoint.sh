cddcc2d8c0eef97b44fb34387824087187d0f279
Nathalie Casati <nathalie.casati@chuv.ch>
2022-03-17 23:42:43 +0000

    Added keycloak authentication

--- services/server/config/docker-entrypoint.sh
+++ services/server/config/docker-entrypoint.sh
@@ -29,5 +29,11 @@ rm -rf /tmp/.X80-lock
 
 # start xpra as $XPRA_USER
 #CMD="XPRA_PASSWORD=$XPRA_PASSWORD /usr/bin/xpra start --daemon=no --start-child='$@'"
-runuser -l $XPRA_USER -c 'pulseaudio --start; pulseaudio --kill; xpra start :80 --bind-tcp=0.0.0.0:8080 --html=on --no-daemon --start="xhost +"'
+#runuser -l $XPRA_USER -c "pulseaudio --start; pulseaudio --kill; xpra start :80 --bind-tcp=0.0.0.0:8080,auth=password:value=mysecret --html=on --no-daemon --start='xhost +' -d auth"
 #runuser -l $XPRA_USER -c 'sleep 1000000000000'
+if [ $XPRA_KEYCLOAK_AUTH = "yes" ]; then
+  AUTH=",auth=keycloak"
+fi
+
+#runuser -l $XPRA_USER -c "pulseaudio --start; pulseaudio --kill; XPRA_KEYCLOAK_SERVER_URL=$XPRA_KEYCLOAK_SERVER_URL XPRA_KEYCLOAK_REALM_NAME=$XPRA_KEYCLOAK_REALM_NAME XPRA_KEYCLOAK_CLIENT_ID=$XPRA_KEYCLOAK_CLIENT_ID XPRA_KEYCLOAK_CLIENT_SECRET_KEY=$XPRA_KEYCLOAK_CLIENT_SECRET_KEY XPRA_KEYCLOAK_REDIRECT_URI=$XPRA_KEYCLOAK_REDIRECT_URI XPRA_KEYCLOAK_SCOPE=$XPRA_KEYCLOAK_SCOPE XPRA_KEYCLOAK_GRANT_TYPE=$XPRA_KEYCLOAK_GRANT_TYPE xpra start :80 --bind-tcp=0.0.0.0:8080$AUTH --html=on --no-daemon --start='xhost +' -d auth"
+runuser -l $XPRA_USER -c "pulseaudio --start; pulseaudio --kill; XPRA_KEYCLOAK_SERVER_URL=$XPRA_KEYCLOAK_SERVER_URL XPRA_KEYCLOAK_REALM_NAME=$XPRA_KEYCLOAK_REALM_NAME XPRA_KEYCLOAK_CLIENT_ID=$XPRA_KEYCLOAK_CLIENT_ID XPRA_KEYCLOAK_CLIENT_SECRET_KEY=$XPRA_KEYCLOAK_CLIENT_SECRET_KEY XPRA_KEYCLOAK_REDIRECT_URI=$XPRA_KEYCLOAK_REDIRECT_URI XPRA_KEYCLOAK_SCOPE=$XPRA_KEYCLOAK_SCOPE XPRA_KEYCLOAK_GRANT_TYPE=$XPRA_KEYCLOAK_GRANT_TYPE xpra start :80 --bind-tcp=0.0.0.0:8080$AUTH --html=on --no-daemon --start='xhost +'"
