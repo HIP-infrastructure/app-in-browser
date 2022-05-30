afcc63ce4f6c73021cf340056983c877e698e7aa
Nathalie Casati <nathalie.casati@chuv.ch>
2022-05-30 13:00:15 +0000

    added support for jupyterlab-desktop

--- services/scripts/process-healthcheck.sh
+++ services/scripts/process-healthcheck.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 # script to test if $PROCESS_NAME is running
 
-if [ $APP_SHELL == "yes" ]; then
+if [ $APP_SPECIAL == "terminal" ]; then
   PROCESS_NAME="/opt/Hyper/hyper"
   APP_NAME="hyper"
+  APP_CMD="hyper"
   CARD=none
+elif [ $APP_SPECIAL == "jupyterlab-desktop" ]; then
+  PROCESS_NAME="electron"
+  APP_NAME="jupyterlab-desktop"
+  APP_CMD="/apps/jupyterlab-desktop/node_modules/electron/dist/electron --no-sandbox /apps/jupyterlab-desktop"
 fi
 
 PID=`ps ax |grep $PROCESS_NAME | grep -v $0 | awk '{print $1}' | tr '\n' ' '`
