ee7b0dea470aac79aa1f05a82a5f2d7cc7c0766a
Nathalie Casati <nathalie.casati@chuv.ch>
2022-09-30 13:39:07 +0000

    fix in case ghostfs fails to authenticate

--- services/scripts/mount-ghostfs.sh
+++ services/scripts/mount-ghostfs.sh
@@ -19,17 +19,17 @@ echo "done."
 echo -n "Mounting ${NEXTCLOUD_DOMAIN} for ${HIP_USER} as ghostfs... "
 #rm -f /var/run/mount.davfs/home-hipuser-nextcloud.pid
 
-CMD="GhostFS --client --host $NEXTCLOUD_HOST --port $NEXTCLOUD_PORT --cert /apps/ghostfs/secrets/cert.pem -o big_writes -o large_read -o allow_root --write-back 32 --read-ahead 0 --user $HIP_USER --token $HIP_PASSWORD /home/$HIP_USER/nextcloud &"
+CMD="GhostFS --client --host $NEXTCLOUD_HOST --port $NEXTCLOUD_PORT --cert /apps/ghostfs/secrets/cert.pem -o big_writes -o large_read -o allow_root --write-back 32 --read-ahead 0 --user $HIP_USER --token $HIP_PASSWORD /home/$HIP_USER/nextcloud & echo \$! > /tmp/ghostfs_status"
 #CMD="GhostFS --client --host $NEXTCLOUD_HOST --port $NEXTCLOUD_PORT -o big_writes -o large_read -o allow_root --write-back 32 --read-ahead 32 --user $HIP_USER --token $HIP_PASSWORD /home/$HIP_USER/nextcloud &"
 runuser -l $HIP_USER -c "$CMD"
 
-retVal=$?
-if [ $retVal -ne 0 ]; then
-    echo "failed."
-    exit $retVal
+sleep 2
+
+if ! ps -p $(cat /tmp/ghostfs_status) > /dev/null
+then
+   echo "failed."
+   exit 1
 fi
 echo "done."
-
-sleep 2
 
 exit 0
