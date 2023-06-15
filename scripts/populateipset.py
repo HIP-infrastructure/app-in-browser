fe5506a304902979c7a45cb867ea6fb9de6aace2
Nathalie Casati <nathalie.casati@chuv.ch>
2023-06-15 12:11:48 +0000

    fixup for template and populateipset

--- scripts/populateipset.py
+++ scripts/populateipset.py
@@ -10,24 +10,30 @@ with open('hip.config.yml') as f:
 #getting the backend whitelist
 try:
   backend_whitelist=hip_config['backend']['whitelist']
-
-  #populating the ipset with the backend whitelist
-  for elem in backend_whitelist:
-      print(f"Whitelisting {elem['comment']} with IP {elem['ip']}...")
-      ret_val = subprocess.check_call(["sudo", "ipset", "add", "docker-allowed", \
-                                     elem['ip'], "comment", elem['comment']])
 except:
   print(f"Not whitelisting any backend IPs since no backend whitelist was found in hip.config.yml")
+
+#populating the ipset with the backend whitelist
+for elem in backend_whitelist:
+  try:
+    print(f"Whitelisting {elem['comment']} with IP {elem['ip']}...")
+    ret_val = subprocess.check_call(["sudo", "ipset", "add", "docker-allowed", \
+                                     elem['ip'], "comment", elem['comment']])
+  except:
+    print(f"Not whitelisting {elem} since it's already whitelisted")
 
 #getting the apps whitelist
 try:
   apps_whitelist=hip_config['apps']['whitelist']
-
-  #populating the ipset with the backend whitelist
-  for app in apps_whitelist.values():
-    for elem in app:
-        print(f"Whitelisting {elem['comment']} with IP {elem['ip']}...")
-        ret_val = subprocess.check_call(["sudo", "ipset", "add", "docker-allowed", \
-                                       elem['ip'], "comment", elem['comment']])
 except:
   print(f"Not whitelisting any apps IPs since no apps whitelist was found in hip.config.yml")
+
+#populating the ipset with the backend whitelist
+for app in apps_whitelist.values():
+  for elem in app:
+    try:
+      print(f"Whitelisting {elem['comment']} with IP {elem['ip']}...")
+      ret_val = subprocess.check_call(["sudo", "ipset", "add", "docker-allowed", \
+                                       elem['ip'], "comment", elem['comment']])
+    except:
+      print(f"Not whitelisting {elem} since it's already whitelisted")
