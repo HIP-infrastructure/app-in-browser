5f14e5392aa6a209acb577a6bc2ceb3c2b80a9b6
HIP Admin <nathalie.casati@chuv.ch>
2023-08-25 09:16:21 +0000

    submodule fixup

--- build/submodule_branch.sh
+++ build/submodule_branch.sh
@@ -4,7 +4,9 @@ for i in services/apps/*/; do
   echo "Updating $i..."
   cd $i;
   git checkout $1
-  git config pull.rebase false
+  if [ -z "${CI_REGISTRY}" ]; then
+    git config pull.rebase false
+  fi
   git pull origin $1
   cd - > /dev/null
   echo "Done with $i."
