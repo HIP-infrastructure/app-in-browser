22099facdd16520e97ec0335ada2d7a12c9309c4
Jonathan Haab <jonathan.haab@chuv.ch>
2025-04-22 15:16:20 +0200

    do not overwrite images by default

--- scripts/buildapp.py
+++ scripts/buildapp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import subprocess
 import argparse
 import yaml
@@ -9,70 +10,83 @@ from dotenv import dotenv_values
 # parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("name", help="name of the app to build")
-parser.add_argument('version', nargs='?', help="version of the app to build")
+parser.add_argument("version", nargs="?", help="version of the app to build")
+parser.add_argument("-f", "--force", default=False, action=argparse.BooleanOptionalAction,
+                    help="overwrite images already found in the registry")
 args = parser.parse_args()
 
 version=args.version
 
 #loading hip.yaml
-with open('hip.yml') as f:
+with open("hip.yml") as f:
   hip = yaml.load(f, Loader=yaml.FullLoader)
 #loading hip.config.yaml
-with open('hip.config.yml') as f:
+with open("hip.config.yml") as f:
   hip_config = yaml.load(f, Loader=yaml.FullLoader)
 
 #if version is not defined get it from hip.yml
 if not version:
-  if hip['apps'][args.name]['version']:
-    version=hip['apps'][args.name]['version']
+  if hip["apps"][args.name]["version"]:
+    version=hip["apps"][args.name]["version"]
   else:
     print(f"Failed to build {args.name} because it wasn't found in hip.yml")
     exit(1)
 
 #getting the dockerfs type
-if hip_config['base']['dockerfs']['type']:
-  dockerfs_type=hip_config['base']['dockerfs']['type']
+if hip_config["base"]["dockerfs"]["type"]:
+  dockerfs_type=hip_config["base"]["dockerfs"]["type"]
 else:
   print(f"Failed to build {args.name} because dockerfs type wasn't found in hip.config.yml")
   exit(1)
 #getting the dockerfs version
-if hip['base'][dockerfs_type]['version']:
-  dockerfs_version=hip['base'][dockerfs_type]['version']
+if hip["base"][dockerfs_type]["version"]:
+  dockerfs_version=hip["base"][dockerfs_type]["version"]
 else:
   print(f"Failed to build {args.name} because dockerfs version wasn't found in hip.yml")
   exit(1)
 
 # load variables from env
-ci_registry_image = os.getenv('CI_REGISTRY_IMAGE')
+ci_registry_image = os.getenv("CI_REGISTRY_IMAGE")
 ci_registry = os.getenv("CI_REGISTRY", "")
-ci_commit_branch = os.getenv('CI_COMMIT_BRANCH')
+ci_commit_branch = os.getenv("CI_COMMIT_BRANCH")
 
 # get ci_registry_image from hip.config.yml in case it is not defined in env
 if not ci_registry_image:
-  if hip_config['backend']['ci']['registry']['image']:
-    ci_registry_image=hip_config['backend']['ci']['registry']['image']
+  if hip_config["backend"]["ci"]["registry"]["image"]:
+    ci_registry_image=hip_config["backend"]["ci"]["registry"]["image"]
   else:
     print(f"Failed to build {args.name} because CI registry image wasn't found in hip.config.yml")
     exit(1)
 
 # get ci_commit_branch from hip.config.yml in case it is not defined in env
 if not ci_commit_branch:
-  if hip_config['backend']['ci']['commit_branch']:
-    ci_commit_branch=hip_config['backend']['ci']['commit_branch']
+  if hip_config["backend"]["ci"]["commit_branch"]:
+    ci_commit_branch=hip_config["backend"]["ci"]["commit_branch"]
   else:
-    print(f"Failed to build {name} because CI registry image wasn't found in hip.config.yml")
+    print(f"Failed to build {args.name} because CI registry image wasn't found in hip.config.yml")
     exit(1)
 
 # create a tag
 if ci_commit_branch != "master":
   tag = f"-{ci_commit_branch}"
 else:
-  tag = ''
+  tag = ""
 
 # define some needed variables
-context = './services'
+context = "./services"
 image = f"{args.name}:{version}{tag}"
 registry_image = f"{ci_registry_image}/{image}"
+
+# check if this specific image:version-tag already exists in the registry
+image_already_exists = (subprocess.run(["docker", "manifest", "inspect", registry_image],
+                         capture_output=True).returncode == 0)
+
+if image_already_exists and not args.force:
+  print(f"{image} skipped, already found in registry \n \
+        Use 'force' to overwrite existing images")
+  sys.exit(0)
+elif image_already_exists and args.force:
+   print(f"Overwriting {image} found on the registry ('force' option used)")
 
 # get app specific build-args
 app_env_path=f"{context}/apps/{args.name}/build.env"
@@ -89,16 +103,16 @@ if ci_registry:
     print(f"Failed pulling {registry_image} from registry.")
 
 # get version of dependencies
-dcm2niix_version = hip['apps']['dcm2niix']['version']
-anywave_version = hip['apps']['anywave']['version']
-freesurfer_version = hip['apps']['freesurfer']['version']
-fsl_version = hip['apps']['fsl']['version']
-brainvisa_version = hip['apps']['brainvisa']['version']
-jupyterlab_desktop_version = hip['base']['jupyterlab-desktop']['version']
-matlab_desktop_version = hip['base']['matlab-desktop']['version']
-terminal_version = hip['base']['terminal']['version']
-virtualgl_version = hip['base']['virtualgl']['version']
-ghostfs_version = hip['base']['ghostfs']['version']
+dcm2niix_version = hip["apps"]["dcm2niix"]["version"]
+anywave_version = hip["apps"]["anywave"]["version"]
+freesurfer_version = hip["apps"]["freesurfer"]["version"]
+fsl_version = hip["apps"]["fsl"]["version"]
+brainvisa_version = hip["apps"]["brainvisa"]["version"]
+jupyterlab_desktop_version = hip["base"]["jupyterlab-desktop"]["version"]
+matlab_desktop_version = hip["base"]["matlab-desktop"]["version"]
+terminal_version = hip["base"]["terminal"]["version"]
+virtualgl_version = hip["base"]["virtualgl"]["version"]
+ghostfs_version = hip["base"]["ghostfs"]["version"]
 
 #build app with cache from registry during CI only
 ret_val = subprocess.check_call(["docker", "build", "--build-arg", f"CI_REGISTRY_IMAGE={ci_registry_image}", \
